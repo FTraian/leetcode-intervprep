34ff28806935d386d0f28da87e7d54f04fefa76e
Traian Fratean <traian.fratean@gmail.com>
2021-03-11 09:19:02 +0200

    https://leetcode.com/problems/first-bad-version

--- src/fb/first_bad_version/Solution.java
+++ src/fb/first_bad_version/Solution.java
@@ -3,10 +3,17 @@ package fb.first_bad_version;
 public class Solution {
 
     public int firstBadVersion(int n) {
-        for(int i = n; i >=0; i--) {
-            if (!isBadVersion(i)) return i + 1;
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = left + (left - right) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
         }
-        return -1;
+
+        return left;
     }
 
     private boolean isBadVersion(int i) {
