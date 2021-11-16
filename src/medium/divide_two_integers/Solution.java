b6ac722e435dc93d5b9371a8f6f38e51e819de50
Traian Fratean <ftraian@gmail.com>
2021-11-16 16:03:17 +0200

    Works on my machine

---
+++ src/medium/divide_two_integers/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+        double sign = 1;
+        if (dividend < 0) {
+            dividend = -dividend;
+            sign = -1;
+        }
+        if (divisor < 0) {
+            divisor = -divisor;
+            sign *= -1;
+        }
+        return (int)(sign * Math.floor(Math.exp(Math.log(dividend) - Math.log(divisor))));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().divide(-2147483647, -1));
+    }
+}
