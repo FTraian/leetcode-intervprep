962b2fe43fe1300ff96f28560660fe326c696677
Traian Fratean <ftraian@gmail.com>
2021-11-25 14:39:44 +0200

    still O(n^2)

--- src/google/maximum_subarray/SolutionMaxSubarray.java
+++ src/google/maximum_subarray/SolutionMaxSubarray.java
@@ -5,7 +5,7 @@ package maximum_subarray;
  */
 public class SolutionMaxSubarray {
 
-    public int maxSubArray(int[] nums) {
+    public int maxSubArrayOnSquared(int[] nums) {
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < nums.length; i++) {
             int partialSum = 0;
@@ -15,6 +15,36 @@ public class SolutionMaxSubarray {
             }
         }
         return max;
+    }
+
+    public int maxSubArray(int[] nums) {
+        int start = 0, end = nums.length - 1;
+        while(nums[start] < 0 && start < nums.length - 1) {
+            start++;
+        }
+        if (start == end) {
+            start = 0;
+            end = nums.length - 1;
+        } else {
+            while (nums[end] < 0 && end > 1) {
+                end--;
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int i = start; i <= end; i++) {
+            int partialSum = 0;
+            for (int j = i; j <= end; j++) {
+                partialSum += nums[j];
+                max = Math.max(max, partialSum);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new SolutionMaxSubarray().maxSubArray(new int[]{-1, -3, -2}));
+        System.out.println(new SolutionMaxSubarray().maxSubArray(new int[]{1}));
     }
 
 }
