8b672c94fdbf0d602f740133558a1d018afaf9df
Traian Fratean <ftraian@gmail.com>
2021-11-25 17:02:22 +0200

    Solution - greedy

--- src/google/maximum_subarray/SolutionMaxSubarray.java
+++ src/google/maximum_subarray/SolutionMaxSubarray.java
@@ -17,7 +17,7 @@ public class SolutionMaxSubarray {
         return max;
     }
 
-    public int maxSubArray(int[] nums) {
+    public int maxSubArrayOnSquaredOptimised(int[] nums) {
         int start = 0, end = nums.length - 1;
         while(nums[start] < 0 && start < nums.length - 1) {
             start++;
@@ -43,8 +43,25 @@ public class SolutionMaxSubarray {
     }
 
     public static void main(String[] args) {
+        System.out.println(new SolutionMaxSubarray().maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
         System.out.println(new SolutionMaxSubarray().maxSubArray(new int[]{-1, -3, -2}));
         System.out.println(new SolutionMaxSubarray().maxSubArray(new int[]{1}));
+    }
+
+    private int maxSubArray(int[] nums) {
+        // Initialize our variables using the first element.
+        int currentSubarray = nums[0];
+        int maxSubarray = nums[0];
+
+        // Start with the 2nd element since we already used the first one.
+        for (int i = 1; i < nums.length; i++) {
+            int num = nums[i];
+            // If current_subarray is negative, throw it away. Otherwise, keep adding to it.
+            currentSubarray = Math.max(num, currentSubarray + num);
+            maxSubarray = Math.max(maxSubarray, currentSubarray);
+        }
+
+        return maxSubarray;
     }
 
 }
