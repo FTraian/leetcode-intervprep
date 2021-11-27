e21266a6a83c590b81813fd29f6b816f50bdbd9e
Traian Fratean <ftraian@gmail.com>
2021-11-27 11:19:00 +0200

    Solution

--- src/google/rotate_image/Solution.java
+++ src/google/rotate_image/Solution.java
@@ -6,31 +6,14 @@ package rotate_image;
 public class Solution {
 
     public void rotate(int[][] matrix) {
-        int t = -1, t1;
-        int length = matrix.length - 1;
-        for (int row  = 0; row <= matrix.length / 2; row++) {
-            for (int column = 0; column <= matrix.length / 2; column++) {
-                for (int j = 0; j < 4; j++) {
-                    switch (j) {
-                        case 0:
-                            t = matrix[row][length - row];
-                            matrix[row][length - row] = matrix[row][row];
-                            break;
-                        case 1:
-                            t1 = matrix[length - row][length - row];
-                            matrix[length - row][length - row] = t;
-                            t = t1;
-                            break;
-                        case 2:
-                            t1 = matrix[length - row][row];
-                            matrix[length - row][row] = t;
-                            t = t1;
-                            break;
-                        case 3:
-                            matrix[row][row] = t;
-                            break;
-                    }
-                }
+        int n = matrix.length;
+        for (int i = 0; i < (n + 1) / 2; i ++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
             }
         }
     }
