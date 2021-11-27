17a2432b39f0036f291b12f59ec0d28b4a314485
Traian Fratean <ftraian@gmail.com>
2021-11-27 14:14:40 +0200

    Solved

--- src/google/three_sum/Solution.java
+++ src/google/three_sum/Solution.java
@@ -1,6 +1,5 @@
 package three_sum;
 
-import javax.swing.*;
 import java.util.*;
 
 /**
@@ -11,32 +10,37 @@ public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Set<List<Integer>> result = new HashSet<>();
 
-//        HashMap<Integer, Integer> map = new HashMap<>();
-//        for (int i = 0; i < nums.length; i++) {
-//            Integer val = map.get(nums[i]);
-//            if (val == null) {
-//                map.put(nums[i], 1);
-//            } else {
-//                map.put(nums[i], ++val);
-//            }
-//        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            Integer val = map.get(nums[i]);
+            if (val == null) {
+                map.put(nums[i], 1);
+            } else {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            }
+        }
 
         for (int i = 0; i < nums.length - 2; i++) {
             for (int j = i + 1; j < nums.length - 1; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    if ( nums[i] +  nums[j] + nums[k] == 0){
-                        List<Integer> list = Arrays.asList(nums[i], nums[j], nums[k]);
-                        list.sort(Integer::compareTo);
-                        result.add(list);
-                    }
+
+                int complement = -nums[i] - nums[j];
+                int noOfComplementsNeeded = 1;
+                if (nums[i] == complement) noOfComplementsNeeded++;
+                if (nums[j] == complement) noOfComplementsNeeded++;
+                Integer elInMap = map.get(complement);
+                if (elInMap != null && elInMap >= noOfComplementsNeeded) {
+                    List<Integer> list = Arrays.asList(nums[i], nums[j], complement);
+                    list.sort(Integer::compareTo);
+                    result.add(list);
                 }
-//                if (map.get(-nums[i] - nums[j]) != null) {
-//                    result.add(List.of(nums[i], nums[j], -nums[i] - nums[j]));
-//                }
             }
         }
 
         return List.copyOf(result);
     }
 
+
+    public static void main(String[] args) {
+        new Solution().threeSum(new int[]{0, 0, 0, 0});
+    }
 }
