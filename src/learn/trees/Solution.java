80e7e4c83fcab15dace510959182d621c162ac22
Traian Fratean <ftraian@gmail.com>
2021-11-28 10:56:54 +0200

    path sum

--- src/learn/trees/Solution.java
+++ src/learn/trees/Solution.java
@@ -52,8 +52,14 @@ public class Solution {
         return result;
     }
 
-    public boolean hasPathSum(TreeNode root, int targetSum) {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null)
+            return false;
 
+        sum -= root.val;
+        if ((root.left == null) && (root.right == null))
+            return (sum == 0);
+        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
     }
 
     public boolean isSymmetric(TreeNode root) {
