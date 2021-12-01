a5031511cfb314523176843d096c90bc0bd43d27
Traian Fratean <ftraian@gmail.com>
2021-12-01 19:04:58 +0200

    this is the ideea

--- src/hard/lru_cache/LRUQuickCache.java
+++ src/hard/lru_cache/LRUQuickCache.java
@@ -1,2 +1,14 @@
-package lru_cache;public class LRUQuickCache {
+package lru_cache;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUQuickCache extends LinkedHashMap<Integer, Integer> {
+
+    private int capacity;
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
+    }
 }
