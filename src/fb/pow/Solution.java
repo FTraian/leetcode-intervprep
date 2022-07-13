afc6dc857e8cc23a78c007b9a4f35da01c6d4715
Traian Fratean <traian@Traians-MBP.lan>
2022-07-13 09:11:01 +0300

    New pow algorithm

--- src/fb/pow/Solution.java
+++ src/fb/pow/Solution.java
@@ -1,5 +1,7 @@
 package fb.pow;
 
+import javax.lang.model.util.ElementScanner14;
+
 public class Solution {
 
     public double myPow(double x, int n) {
@@ -26,8 +28,17 @@ public class Solution {
         }
     }
 
+    public double myPowWiki(double x, int n) {
+        // stack overflow for large numbers ?!?
+        if (n < 0) return myPowWiki(1/x, -n);
+        else if (n == 0) return 1;
+        else if (n % 2 == 0) return myPowWiki(x * x, n / 2);
+        else return x * myPowWiki(x * x, (n - 1) / 2);
+    }
+
     public static void main(String[] args) {
         System.out.println(new Solution().myPow(2, -2147483648));
+        System.out.println(new Solution().myPowWiki(2, -2147483648));
         System.out.println(Math.pow(2, -2147483648));
     }
 
